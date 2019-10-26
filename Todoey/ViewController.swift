20de9e7ecff5a3b9e8743dbda5c21c659db46024
Joseph Calla <josephestanislao9@gmail.com>
2019-10-26 18:09:21 -0500

    Todo List complete with datasource and delegate methods

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Joseph Estanislao Calla Moreyra on 10/26/19.
+//  Copyright © 2019 Joseph Estanislao Calla Moreyra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        <#code#>
+    }
+
+    
+}
+
